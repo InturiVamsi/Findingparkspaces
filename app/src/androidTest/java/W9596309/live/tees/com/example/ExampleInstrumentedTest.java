bc897e73946aa75eaa5b25e0514f939a5862af98
Vamsi Krishna INTURI <w9596309@linux66.scedt.tees.ac.uk>
2023-02-28 15:58:54 +0000

    Initial commit

---
+++ app/src/androidTest/java/W9596309/live/tees/com/example/ExampleInstrumentedTest.java
@@ -0,0 +1,25 @@
+package W9596309.live.tees.com.example;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("W9596309.live.tees.com.example", appContext.getPackageName());
+    }
+}
